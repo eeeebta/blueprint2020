686724314c3626f9528287563b38701af319b9a8
eeeebta <eeeebta@gmail.com>
2020-02-09 11:13:35 -0500

    Added barebones

--- app.py
+++ app.py
@@ -3,7 +3,19 @@ import spotipy
 app = Flask(__name__)
 
 
+# Quiz at start or separate?
+# Index -> quiz?
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+# If different
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
+
+
+@app.route("/make")
+def create_playlist():
+    return render_template("make.html")
