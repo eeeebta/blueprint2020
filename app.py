40be96c61d1b255153f09504526d297571d51457
eeeebta <eeeebta@gmail.com>
2020-02-09 09:56:22 -0500

    Example

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route("test")
+def test():
+    return render_template("index.html")
